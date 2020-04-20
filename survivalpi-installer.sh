0187aafa91ad0620b2c4536eba6d6dffa5fa59f5
Brian Chabot <brian.chabot@vistaprint.com>
2020-04-20 17:49:41 +0000

    Update survivalpi-installer.sh

---
+++ survivalpi-installer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script is meant to be run on a fresh installation of the 
+# latest version of latest Raspian Lite.
+# Usage: If you have internet, get the latest version from:
+#     wget https://URL | sudo bash
+# Run this as ROOT!
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   echo "Please use sudo."
+   exit 1
+fi
+# Config or interactive?  
+
+# Run updates.
+apt update -y
+apt upgrade -y
+
+# Set hostname
+
+# Create an Admin User
+
+# Set up DNS (Local and caching)
+# including .local as default.
+
+# Set up DHCP
+
+# Set up WiFi 
+
+# Install LAMP stack
+
+# Install WordPress
+#    WP-multi? https://wordpress.org/support/article/create-a-network/
+
+# Install Media Wiki
+
+# Install NextCloud
+
+# Install Email?
+
+# Install a social media network....
+#    OSSN? https://www.opensource-socialnetwork.org/
+#    ELGG/PLIGG?
+#    BuddyPress? 
+#    Dolphin?
+#    b2evo?
